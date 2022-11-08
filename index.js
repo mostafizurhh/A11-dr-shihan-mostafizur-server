f9da1167115a0cfc6ccb790b1ea337c932225d4a
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-08 13:09:27 +0000

    initial server setup done

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+const port = process.env.PORT || 5000
+
+require('dotenv').config() /* to hide DB credential */
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+})
+
+app.listen(port, () => {
+    console.log(`listening from port ${port}`)
+})
