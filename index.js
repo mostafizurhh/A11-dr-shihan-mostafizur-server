6b1389ea74216764c0e2e6c84566ae6494f1498d
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-08 21:08:28 +0000

    service API created

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT || 5000
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config() /* to hide DB credential */
 
 app.use(cors())
@@ -16,3 +16,104 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`listening from port ${port}`)
 })
+
+/*--------------------
+  mongoDB connection 
+  -------------------*/
+/* hide DB credential */
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        /* create a DB in mongoDB for all services */
+        const serviceCollection = client.db('dr-shihan').collection('services');
+
+        /* to show DB's data to UI */
+        /* API to get all services */
+        app.get('/services', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.limit(3).toArray()
+            res.send(services)
+        });
+        app.get('/allservices', async (req, res) => {
+            const query = {}
+            const cursor = serviceCollection.find(query)
+            const services = await cursor.toArray()
+            res.send(services)
+        });
+
+        /* API to get a specific service/data */
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service)
+        });
+
+        /* create JWT token API */
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+            res.send({ token })
+        })
+
+        // /* create a DB in mongoDB for all orders */
+        // const orderCollection = client.db('cool-car').collection('orders');
+
+        // /* (CREATE)create single single data from client side info */
+        // app.post('/orders', verifyJWT, async (req, res) => {
+        //     const order = req.body;
+        //     const result = await orderCollection.insertOne(order);
+        //     res.send(result);
+        // })
+
+        // /* (READ)create API to get all orders data */
+        // app.get('/orders', verifyJWT, async (req, res) => {
+        //     /* verify user with jwt token */
+        //     const decoded = req.decoded;
+        //     console.log(decoded)
+        //     if (decoded.email !== req.query.email) {
+        //         return res.status(403).send({ message: 'Forbidden User' })
+        //     }
+        //     let query = {}
+        //     /* find specific user's order with email */
+        //     if (req.query.email) {
+        //         query = {
+        //             email: req.query.email
+        //         }
+        //     }
+        //     const cursor = orderCollection.find(query)
+        //     const orders = await cursor.toArray()
+        //     res.send(orders)
+        // });
+
+        // /* (UPDATE) create API to partially update a specific data from server and DB */
+        // app.patch('/orders/:id', verifyJWT, async (req, res) => {
+        //     const id = req.params.id;
+        //     const status = req.body.status;
+        //     const query = { _id: ObjectId(id) }
+        //     const updatedDoc = {
+        //         $set: {
+        //             status: status
+        //         }
+        //     }
+        //     const result = await orderCollection.updateOne(query, updatedDoc);
+        //     res.send(result)
+        // });
+
+        // /* (DELETE) create API to delete a specific data from server and DB */
+        // app.delete('/orders/:id', verifyJWT, async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) }
+        //     const result = await orderCollection.deleteOne(query)
+        //     res.send(result)
+        // });
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error(error))
