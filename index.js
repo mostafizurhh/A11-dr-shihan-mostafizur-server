32abafa8ba4789a2266dd21872652b68bc2486d4
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-09 20:43:08 +0000

    created api to get specific review by id

--- index.js
+++ index.js
@@ -79,6 +79,14 @@ async function run() {
             res.send(reviews)
         });
 
+        /* (UPDATE) create API to update a specific review data from server and DB */
+        app.get('/reviews/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await reviewCollection.findOne(query);
+            res.send(result)
+        });
+
         /* (DELETE) create API to delete a specific review data from server and DB */
         app.delete('/reviews/:id', async (req, res) => {
             const id = req.params.id;
