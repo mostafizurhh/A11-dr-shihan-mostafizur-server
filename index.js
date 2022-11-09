bdbadb72978e49179120c8da59fd9b5fe6a76534
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-11-09 22:11:58 +0000

    created JWT token and applied to client and server side

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors')
-
+const jwt = require('jsonwebtoken')
 const app = express()
 const port = process.env.PORT || 5000
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
@@ -22,8 +22,25 @@ app.listen(port, () => {
   -------------------*/
 /* hide DB credential */
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mniec4l.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
+
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+/* verify and decode JWT token from client */
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    console.log(authHeader)
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized Access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden User' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 
 async function run() {
     try {
@@ -53,20 +70,31 @@ async function run() {
             res.send(service)
         });
 
-
+        /* create JWT token API */
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '7d' })
+            res.send({ token })
+        })
 
         /* create a DB in mongoDB for all reviews */
         const reviewCollection = client.db('dr-shihan').collection('reviews');
 
         /* (CREATE)create single single data from client side info */
-        app.post('/reviews', async (req, res) => {
+        app.post('/reviews', verifyJWT, async (req, res) => {
             const review = req.body;
             const result = await reviewCollection.insertOne(review);
             res.send(result);
         })
 
         /* (READ)create API to get all reviews data from DB*/
-        app.get('/reviews', async (req, res) => {
+        app.get('/reviews', verifyJWT, async (req, res) => {
+            /* verify user with jwt token */
+            const decoded = req.decoded;
+
+            if (decoded.email !== req.query.email) {
+                return res.status(403).send({ message: 'Forbidden User' })
+            }
             let query = {}
             /* find specific user's review with email */
             if (req.query.email) {
@@ -88,7 +116,7 @@ async function run() {
         });
 
         /* (UPDATE) create API to update a specific review data from server and DB */
-        app.patch('/reviews/:id', async (req, res) => {
+        app.patch('/reviews/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
             const review = req.body;
@@ -102,7 +130,7 @@ async function run() {
         });
 
         /* (DELETE) create API to delete a specific review data from server and DB */
-        app.delete('/reviews/:id', async (req, res) => {
+        app.delete('/reviews/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) }
             const result = await reviewCollection.deleteOne(query)
